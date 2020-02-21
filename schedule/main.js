f06b6617647c8023a262b970839abf8c7916a210
Ian Park <contact@duckonomy.com>
2020-02-21 16:00:53 -0500

    schedule

--- schedule/main.js
+++ schedule/main.js
@@ -433,6 +433,7 @@ function hey() {
   alert("Not Logged In!");
 }
 
+
  /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function myFunction() {
   var x = document.getElementById("mob-links");
